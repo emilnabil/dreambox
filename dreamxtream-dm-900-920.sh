8aba4287a50766a93e31c93e56b41cdf695a1f69
emil-nabil333 <elbrinselomda@yahoo.com>
2025-07-29 11:00:16 +0300

    Add files via upload

--- dreamxtream-dm-900-920.sh
+++ dreamxtream-dm-900-920.sh
@@ -1,2 +1,3 @@
-wget http://dreambox4u.com/ch/E2/enigma2-plugin-extensions-dreamxtream_0.3-r2.0_armhf.deb -O /tmp/enigma2-plugin-extensions-dreamxtream_0.3-r2.0_armhf.deb && dpkg -i /tmp/enigma2-plugin-extensions-dreamxtream_0.3-r2.0_armhf.deb
+wget https://github.com/emilnabil/dreambox/raw/refs/heads/main/enigma2-plugin-extensions-dreamxtream_0.3-r2.0_armhf.deb -O /tmp/enigma2-plugin-extensions-dreamxtream_0.3-r2.0_armhf.deb && dpkg -i /tmp/enigma2-plugin-extensions-dreamxtream_0.3-r2.0_armhf.deb
+
 
