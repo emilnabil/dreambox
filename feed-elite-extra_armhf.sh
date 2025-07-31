d7a2af39e7ce2c4188e9102598fed01b1a65cf2c
emil-nabil333 <elbrinselomda@yahoo.com>
2025-07-31 18:17:45 +0300

    Add files via upload

---
+++ feed-elite-extra_armhf.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+##command=wget https://github.com/emilnabil/dreambox/raw/refs/heads/main/feed-elite-extra_armhf.sh -O - | /bin/sh
+#####################
+cd /tmp || exit 1
+
+echo "Downloading plugin package..."
+curl -kL "https://github.com/emilnabil/dreambox/raw/refs/heads/main/feed-elite-extra_armhf.tar.gz" -o /tmp/feed-elite-extra_armhf.tar.gz
+if [ $? -ne 0 ]; then
+    echo "✖ Download failed!"
+    exit 1
+fi
+
+echo "Installing package..."
+tar -xzf /tmp/feed-elite-extra_armhf.tar.gz -C /
+if [ $? -ne 0 ]; then
+    echo "✖ Extraction failed!"
+    exit 1
+fi
+
+echo "Cleaning up..."
+rm -f /tmp/feed-elite-extra_armhf.tar.gz
+
+echo ""
+echo "✅ Installation complete!"
+echo ">>>>>>>>>>>>>>>>> DONE <<<<<<<<<<<<<<<<<"
+sleep 2
+exit 0
+
+
+
+
+
+
+
+
