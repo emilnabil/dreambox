344a611367b448c87888da0af2470d335fb5eef5
emil-nabil333 <elbrinselomda@yahoo.com>
2025-08-01 11:02:27 +0300

    Add files via upload

---
+++ PluginHider.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+##comand=wget https://github.com/emilnabil/dreambox/raw/refs/heads/main/PluginHider.sh -O - | /bin/sh
+#####################
+cd /tmp || exit 1
+
+echo "Downloading plugin package..."
+curl -kL "https://github.com/emilnabil/dreambox/raw/refs/heads/main/PluginHider.tar.gz" -o /tmp/PluginHider.tar.gz
+if [ $? -ne 0 ]; then
+    echo "✖ Download failed!"
+    exit 1
+fi
+
+echo "Installing package..."
+tar -xzf /tmp/PluginHider.tar.gz -C /
+if [ $? -ne 0 ]; then
+    echo "✖ Extraction failed!"
+    exit 1
+fi
+
+echo "Cleaning up..."
+rm -f /tmp/PluginHider.tar.gz
+
+echo ""
+echo "✅ Installation complete!"
+echo ">>>>>>>>>>>>>>>>> DONE <<<<<<<<<<<<<<<<<"
+sleep 2
+exit 0
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ PluginSort.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+##comand=wget https://github.com/emilnabil/dreambox/raw/refs/heads/main/PluginSort.sh -O - | /bin/sh
+#####################
+cd /tmp || exit 1
+
+echo "Downloading plugin package..."
+curl -kL "https://github.com/emilnabil/dreambox/raw/refs/heads/main/PluginSort.tar.gz" -o /tmp/PluginSort.tar.gz
+if [ $? -ne 0 ]; then
+    echo "✖ Download failed!"
+    exit 1
+fi
+
+echo "Installing package..."
+tar -xzf /tmp/PluginSort.tar.gz -C /
+if [ $? -ne 0 ]; then
+    echo "✖ Extraction failed!"
+    exit 1
+fi
+
+echo "Cleaning up..."
+rm -f /tmp/PluginSort.tar.gz
+
+echo ""
+echo "✅ Installation complete!"
+echo ">>>>>>>>>>>>>>>>> DONE <<<<<<<<<<<<<<<<<"
+sleep 2
+exit 0
+
+
+
