ddbdb7d8fc926804b85ffb7a2473a187fdbe33cf
emil-nabil333 <elbrinselomda@yahoo.com>
2025-08-07 10:21:09 +0300

    Add files via upload

---
+++ quickbutton.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+##comand=wget https://github.com/emilnabil/dreambox/raw/refs/heads/main/quickbutton.sh -O - | /bin/sh
+#####################
+cd /tmp || exit 1
+
+echo "Downloading plugin package..."
+curl -kL "https://github.com/emilnabil/dreambox/raw/refs/heads/main/quickbutton.tar.gz" -o /tmp/quickbutton.tar.gz
+if [ $? -ne 0 ]; then
+    echo "✖ Download failed!"
+    exit 1
+fi
+
+echo "Installing package..."
+tar -xzf /tmp/quickbutton.tar.gz -C /
+if [ $? -ne 0 ]; then
+    echo "✖ Extraction failed!"
+    exit 1
+fi
+
+echo "Cleaning up..."
+rm -f /tmp/quickbutton.tar.gz
+
+echo ""
+echo "✅ Installation complete!"
+echo ">>>>>>>>>>>>>>>>> DONE <<<<<<<<<<<<<<<<<"
+sleep 2
+exit 0
+
+
+
