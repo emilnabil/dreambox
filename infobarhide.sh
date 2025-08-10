9e59160f0340f2261c89f6e8eb4bbc37fb3d734b
emil-nabil333 <elbrinselomda@yahoo.com>
2025-08-10 09:54:17 +0300

    Add files via upload

---
+++ infobarhide.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+##comand=wget https://github.com/emilnabil/dreambox/raw/refs/heads/main/infobarhide.sh -O - | /bin/sh
+#####################
+cd /tmp || exit 1
+
+echo "Downloading plugin package..."
+curl -kL "https://github.com/emilnabil/dreambox/raw/refs/heads/main/infobarhide.tar.gz" -o /tmp/infobarhide.tar.gz
+if [ $? -ne 0 ]; then
+    echo "✖ Download failed!"
+    exit 1
+fi
+
+echo "Installing package..."
+tar -xzf /tmp/infobarhide.tar.gz -C /
+if [ $? -ne 0 ]; then
+    echo "✖ Extraction failed!"
+    exit 1
+fi
+
+echo "Cleaning up..."
+rm -f /tmp/infobarhide.tar.gz
+
+echo ""
+echo "✅ Installation complete!"
+echo ">>>>>>>>>>>>>>>>> DONE <<<<<<<<<<<<<<<<<"
+sleep 2
+exit 0
+
+
+
+
