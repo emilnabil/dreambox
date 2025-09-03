5a6de41506dccaa06bd5fcc007ec108f7473ac47
emil-nabil333 <elbrinselomda@yahoo.com>
2025-09-03 12:35:56 +0300

    Add files via upload

---
+++ dreamxtream-dm-1-2.sh
@@ -0,0 +1,5 @@
+wget -O /tmp/enigma2-plugin-extensions-dreamxtream_0.3-r9.0_arm64.deb https://github.com/emilnabil/dreambox/raw/refs/heads/main/enigma2-plugin-extensions-dreamxtream_0.3-r9.0_arm64.deb
+dpkg -i /tmp/enigma2-plugin-extensions-dreamxtream_0.3-r9.0_arm64.deb
+apt-get -f -y install
+
+
