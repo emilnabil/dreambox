a5595affa86011017c915c1a205e3733f289acc5
emil-nabil333 <elbrinselomda@yahoo.com>
2025-09-03 12:36:20 +0300

    Add files via upload

--- dreamxtream-dm-520.sh
+++ dreamxtream-dm-520.sh
@@ -1,3 +1,5 @@
-wget https://github.com/emilnabil/dreambox/raw/refs/heads/main/enigma2-plugin-extensions-dreamxtream_0.3-r2.0_mipsel.deb -O /tmp/enigma2-plugin-extensions-dreamxtream_0.3-r2.0_mipsel.deb && dpkg -i /tmp/enigma2-plugin-extensions-dreamxtream_0.3-r2.0_mipsel.deb
+wget -O /tmp/enigma2-plugin-extensions-dreamxtream_0.3-r2.0_mipsel.deb https://github.com/emilnabil/dreambox/raw/refs/heads/main/enigma2-plugin-extensions-dreamxtream_0.3-r2.0_mipsel.deb
+dpkg -i /tmp/enigma2-plugin-extensions-dreamxtream_0.3-r2.0_mipsel.deb
+apt-get -f -y install
 
 
