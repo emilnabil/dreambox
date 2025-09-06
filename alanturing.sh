49845775580aa5a318a44003755686e2dd8e2f63
emil-nabil333 <elbrinselomda@yahoo.com>
2025-09-06 16:17:28 +0300

    Add files via upload

---
+++ alanturing.sh
@@ -0,0 +1,8 @@
+wget -O /tmp/alanturing_4.2-r9_all.deb https://github.com/emilnabil/dreambox/raw/refs/heads/main/alanturing_4.2-r9_all.deb
+dpkg -i /tmp/alanturing_4.2-r9_all.deb
+sleep 2
+apt-get -f -y install
+
+
+
+
