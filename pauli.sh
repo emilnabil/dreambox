3320690c8a55931dcc7e31dcc313e5c2c98cbcfa
emil-nabil333 <elbrinselomda@yahoo.com>
2025-09-07 12:22:54 +0300

    Add files via upload

--- pauli.sh
+++ pauli.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
-# command=wget https://github.com/emilnabil/dreambox/raw/refs/heads/main/pauli.sh -O - | /bin/sh
+#
+##comand=wget https://github.com/emilnabil/dreambox/raw/refs/heads/main/pauli.sh -O - | /bin/sh
+#######################################
 
-apt update
-wget -O /tmp/enigma2-plugin-extensions-pauli_8.0-r2_all.deb https://github.com/emilnabil/dreambox/raw/refs/heads/main/enigma2-plugin-extensions-pauli_8.0-r2_all.deb
-dpkg -i /tmp/enigma2-plugin-extensions-pauli_8.0-r2_all.deb
-apt install -f -y
-sleep 2
-rm -f/tmp/enigma2-plugin-extensions-pauli_8.0-r2_all.deb
-exit 0
+URL="https://github.com/emilnabil/dreambox/raw/refs/heads/main/pauli_8.0-r2_all.deb"
+DEB_FILE="/tmp/pauli_8.0-r2_all.deb"
+
+wget -O "$DEB_FILE" "$URL"
+if [ $? -ne 0 ]; then
+    echo "Download failed: $URL"
+    exit 1
+fi
+
+dpkg -i "$DEB_FILE"
+if [ $? -ne 0 ]; then
+    echo "Errors occurred during installation, trying to fix..."
+fi
+
+apt-get -f -y install
+
+rm -f "$DEB_FILE"
+exit
+
+
+
+
+
 
 
