5572cada59d57dcf14b4138376b531472897aa13
emil-nabil333 <elbrinselomda@yahoo.com>
2025-09-15 12:59:27 +0300

    Add files via upload

--- disk_and_cpu_temperature.sh
+++ disk_and_cpu_temperature.sh
@@ -1,52 +1,61 @@
 #!/bin/sh
 
-#wget -q --no-check-certificate https://github.com/emilnabil/dreambox/raw/refs/heads/main/disk_and_cpu_temperature.sh -O - | /bin/sh
+# wget -q --no-check-certificate https://raw.githubusercontent.com/emilnabil/dreambox/refs/heads/main/disk_and_cpu_temperature.sh -O - | /bin/sh
 
 ##########################################
 TEMPATH=/tmp
 OPKGINSTALL="opkg install --force-reinstall"
 MY_IPK="enigma2-plugin-extensions-Disk_and_CPU_Temperature_all.ipk"
 MY_DEB="enigma2-plugin-extensions-Disk_and_CPU_Temperature_all.deb"
-MY_URL="https://github.com/emilnabil/dreambox/raw/refs/heads/main"
-# remove old version #
+MY_URL="https://raw.githubusercontent.com/emilnabil/dreambox/refs/heads/main"
+
 if which dpkg > /dev/null 2>&1; then
-	apt-get purge --auto-remove -y enigma2-plugin-extensions-Disk_and_CPU_Temperature &>/dev/null
-	dpkg --purge --force-all enigma2-plugin-extensions-Disk_and_CPU_Temperature &>/dev/null
-	dpkg --remove --force-depends enigma2-plugin-extensions-Disk_and_CPU_Temperature &>/dev/null
-rm -rf /usr/lib/enigma2/python/Plugins/SystemPlugins/DiskCpuTemp
+    apt-get purge --auto-remove -y enigma2-plugin-extensions-Disk_and_CPU_Temperature &>/dev/null
+    dpkg --purge --force-all enigma2-plugin-extensions-Disk_and_CPU_Temperature &>/dev/null
+    dpkg --remove --force-depends enigma2-plugin-extensions-Disk_and_CPU_Temperature &>/dev/null
+
 else
-opkg remove enigma2-plugin-extensions-Disk_and_CPU_Temperature
+    opkg remove enigma2-plugin-extensions-Disk_and_CPU_Temperature
+fi
 rm -rf /usr/lib/enigma2/python/Plugins/SystemPlugins/DiskCpuTemp
 
 echo ""
-# Download and install plugin
-
+echo ">>>> Starting installation <<<<"
 cd /tmp
 
- if which dpkg > /dev/null 2>&1; then
-wget "$MY_URL/$MY_DEB"
-sleep 2 
-		dpkg -i --force-overwrite $MY_DEB; apt-get install -f -y
-sleep 2
-rm -f /tmp/*.deb
-	else
-wget "$MY_URL/$MY_IPK"
-sleep 2 
-		$OPKGINSTALL $MY_IPK
-sleep 2
-rm -f /tmp/*.ipk
-	fi
-echo "================================="
+if which dpkg > /dev/null 2>&1; then
+    wget "$MY_URL/$MY_DEB"
+    sleep 2 
+    dpkg -i --force-overwrite $MY_DEB
+    apt-get install -f -y
+    rm -f /tmp/*.deb
+else
+    wget "$MY_URL/$MY_IPK"
+    sleep 2 
+    $OPKGINSTALL $MY_IPK
+    rm -f /tmp/*.ipk
+fi
+
 cd ..
 
-	if [ $? -eq 0 ]; then
-echo ">>>>  SUCCESSFULLY INSTALLED <<<<"
+if [ $? -eq 0 ]; then
+    echo ">>>>  SUCCESSFULLY INSTALLED <<<<"
+else
+    echo ">>>>  INSTALLATION FAILED <<<<"
 fi
-		echo "********************************************************************************"
+
+echo "********************************************************************************"
 echo "   UPLOADED BY  >>>>   EMIL_NABIL "   
+echo "********************************************************************************"
 sleep 4
-		echo ". >>>>         RESTARING     <<<<"
-echo "**********************************************************************************"
-wait
-killall -9 enigma2
+
+echo ">>>>         RESTARTING ENIGMA2     <<<<"
+if which dpkg > /dev/null 2>&1; then
+    systemctl restart enigma2
+else
+    killall -9 enigma2
+fi
+
 exit 0
+
+
