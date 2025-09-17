7c600ee5090ca82d3964fe39842bb1b0e4b4085e
emil-nabil333 <elbrinselomda@yahoo.com>
2025-09-17 19:52:38 +0300

    Add files via upload

---
+++ bootlogoswaper-gemini.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+## command=curl -kL https://github.com/emilnabil/dreambox/raw/refs/heads/main/bootlogoswaper-gemini.sh | bash
+######################
+set -e 
+TMPFILE="/tmp/BootlogoSwap-Gemini.tar.gz"
+URL="https://github.com/emilnabil/dreambox/raw/refs/heads/main/BootlogoSwap-Gemini.tar.gz"
+
+cd /tmp
+
+echo "Downloading plugin package..."
+curl -kL "$URL" -o "$TMPFILE"
+
+echo "Installing package..."
+tar -xzf "$TMPFILE" -C /
+
+echo "Cleaning up..."
+rm -f "$TMPFILE"
+
+echo ""
+echo "✅ Installation complete!"
+echo ">>>>>>>>>>>>>>>>> DONE <<<<<<<<<<<<<<<<<"
+sleep 2
+exit 0
+
+
