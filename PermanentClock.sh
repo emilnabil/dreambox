e8e4853ee07e9cd07b11422697a347c712472e32
emil-nabil333 <elbrinselomda@yahoo.com>
2025-10-11 07:06:34 +0300

    Add files via upload

---
+++ PermanentClock.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+##comand=wget https://github.com/emilnabil/dreambox/raw/refs/heads/main/PermanentClock.sh -O - | /bin/sh
+#####################
+cd /tmp || exit 1
+
+echo "Downloading plugin package..."
+curl -kL "https://github.com/emilnabil/dreambox/raw/refs/heads/main/PermanentClock.tar.gz" -o /tmp/PermanentClock.tar.gz
+if [ $? -ne 0 ]; then
+    echo "✖ Download failed!"
+    exit 1
+fi
+
+echo "Installing package..."
+tar -xzf /tmp/PermanentClock.tar.gz -C /
+if [ $? -ne 0 ]; then
+    echo "✖ Extraction failed!"
+    exit 1
+fi
+
+echo "Cleaning up..."
+rm -f /tmp/PermanentClock.tar.gz
+
+echo ""
+echo "✅ Installation complete!"
+echo ">>>>>>>>>>>>>>>>> DONE <<<<<<<<<<<<<<<<<"
+sleep 2
+exit 0
+
+
+
+
+
+
+
