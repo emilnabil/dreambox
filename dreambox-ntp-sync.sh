3f7f32733abb0f1a8ebef897fe744cbbb1fd0d13
emil-nabil333 <elbrinselomda@yahoo.com>
2026-08-26 23:02:05 +0300

    Add files via upload

---
+++ dreambox-ntp-sync.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if command -v opkg &> /dev/null; then
+    wget -O /tmp/dreambox-ntp-sync_all.ipk https://github.com/emilnabil/dreambox/raw/refs/heads/main/dreambox-ntp-sync_all.ipk
+    opkg install /tmp/dreambox-ntp-sync_all.ipk
+else
+    wget -O /tmp/dreambox-ntp-sync_all.deb https://github.com/emilnabil/dreambox/raw/refs/heads/main/dreambox-ntp-sync_all.deb
+    dpkg -i /tmp/dreambox-ntp-sync_all.deb
+    sleep 2
+    apt-get -f -y install
+fi
+
+sleep 2
+reboot
+exit 0
